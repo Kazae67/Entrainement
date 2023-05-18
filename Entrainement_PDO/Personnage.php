8350f21a1633d39782f628e2b28e40efae821a61
Kazae67 <Kazae.zaeka@gmail.com>
2023-05-18 20:09:00 +0200

    début : Bien documenter ses classes

--- Entrainement_PDO/Personnage.php
+++ Entrainement_PDO/Personnage.php
@@ -1,6 +1,11 @@
 <?php
 
-// Class
+// Utiliser /** pour documenter efficacement */
+
+/**
+ * Class Form
+ * Permet de générer un formulaire rapidement et simplement
+ */
 Class Personnage{
     private $nom; // private : peut entrer seulement depuis la classe, utiliser "Get" pour récupérer
     private $pv = 100;
