254d6a31e1ed0d1595534060856afae06f82f8ef
Kazae67 <Kazae.zaeka@gmail.com>
2023-05-20 14:45:28 +0200

    Création de la Database.php

--- Entrainement_PDO/pages/home.php
+++ Entrainement_PDO/pages/home.php
@@ -7,7 +7,10 @@
 $pdo = new PDO('mysql:dbname=blog;host=localhost', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // renvoie l'erreur concerné, faire avant un die(var_dump(PDO::ATTR_ERRMODE));
 $resultat = $pdo->query('SELECT * FROM articles'); // query retourne un PDO statement (pour ensuite utiliser un fetchAll(récupérer toutes les données))
-var_dump($resultat->fetchAll()); // fetchAll() récupère toutes les données et nous renvoie sous forme de tableau
+$datas = $resultat->fetchAll(PDO::FETCH_OBJ); // fetchAll() récupère les données renvoie sous forme de tableau | FETCH_OBJ renvoie sous forme d'objet
+var_dump($datas[0]->titre); 
 
-$count = $pdo->exec('INSERT INTO articles SET titre="Mon titre", date="' . date('Y-m-d H:i:s') . '"'); // Exec nous renvoie un entier
-var_dump($count);
+
+// à désactiver pour ne pas créer un nouveau tableau
+// $count = $pdo->exec('INSERT INTO articles SET titre="Mon titre", date="' . date('Y-m-d H:i:s') . '"'); // Exec nous renvoie un entier 
+// var_dump($count);
