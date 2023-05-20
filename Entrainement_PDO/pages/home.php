d7cca735a3b7d2f24bd4c01bc89b4a1d96d095ad
Kazae67 <Kazae.zaeka@gmail.com>
2023-05-20 13:58:32 +0200

    Utilisation du DNS

--- Entrainement_PDO/pages/home.php
+++ Entrainement_PDO/pages/home.php
@@ -1,7 +1,5 @@
 <?php
 
-$pdo = new PDO('mysql:dbname=blog;host=localhost', 'root', 'root');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$res = $pdo->query('SELECT * FROM articles');
-$datas = $res->fetchAll(PDO::FETCH_OBJ);
-var_dump($datas[0]->titre);
+$pdo = new PDO('mysql:dbname=blog;host=localhost', 'root', '');
+$count = $pdo->exec('INSERT INTO articles SET titre="Mon titre", date="' . date('Y-m-d H:i:s') . '"');
+var_dump($count);
